2785bd885df2e5345ca987db4f1eacc66eb11476
Matej <matejpale@gmail.com>
2021-07-03 15:28:47 +0200

    injected stock data to controlled components

--- script.js
+++ script.js
@@ -5,8 +5,31 @@
 class Portfolio extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      portfolio: [
+        {
+          name: "Friendsbook",
+          shares_owned: 20,
+          cost_per_share: 50,
+          market_price: 130,
+        },
+        {
+          name: "Yamazon",
+          shares_owned: 5,
+          cost_per_share: 200,
+          market_price: 500,
+        },
+        {
+          name: "Snoozechat",
+          shares_owned: 100,
+          cost_per_share: 20,
+          market_price: 3,
+        },
+      ],
+    };
   }
   render() {
+    const { portfolio } = this.state;
     return (
       <div className="container">
         <h1 className="text-center my-4">Stock Portfolio</h1>
@@ -24,7 +47,43 @@ class Portfolio extends React.Component {
                   <th scope="col"></th>
                 </tr>
               </thead>
-              <tbody></tbody>
+              <tbody>
+                {portfolio.map((stock, index) => {
+                  const { name, shares_owned, cost_per_share, market_price } =
+                    stock;
+                  return (
+                    <tr key={index}>
+                      <td>{name}</td>
+                      <td>
+                        <input
+                          type="number"
+                          name="shares_owned"
+                          value={shares_owned}
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="number"
+                          name="cost_per_share"
+                          value={cost_per_share}
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type="number"
+                          name="market_price"
+                          value={market_price}
+                        />
+                      </td>
+                      <td></td>
+                      <td></td>
+                      <td>
+                        <button className="btn btn-light btn-sm">Remove</button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
             </table>
           </div>
         </div>
